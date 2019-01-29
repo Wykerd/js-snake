14051161561bec4eec85c4d23a2ed1262fcc0531
Daniel Wykerd <danielwykerd@gmail.com>
2019-01-29 18:30:53 +0200

    Improved UI

--- js/main.js
+++ js/main.js
@@ -2,16 +2,18 @@
 /*      Definitions!      */
 /* ====================== */
 
-const tps = 10; //frames per second. AKA number of times the timer executes per second!
+var tps = 10; //frames per second. AKA number of times the timer executes per second!
 const spawnlength = 3;
-const grid = {
+var grid = {
   width: 31,
   height: 31,
 };
 
-const speedUp = false;
+var game = {};
 
-const color = {
+var speedUp = false;  //document.getElementById('speed-up').checked
+
+var color = { //default color pallet
   player: [
     {
       h: "#44891A",
@@ -20,12 +22,24 @@ const color = {
     {
       h: '#005784',
       t: '#31A2F2'
+    },
+    {
+      h: '#EB8931',
+      t: '#F7E26B'
+    },
+    {
+      h: '#493C2B',
+      t: '#A46422'
+    },
+    {
+      h: '#1B2632',
+      t: '#2F484E'
     }
   ],
   pellet: "#BE2633"
 }
 
-const keybinds = [{
+var keybinds = [{
   up: 87,
   down: 83,
   left: 65,
@@ -169,8 +183,45 @@ function Game(canvas, tps, snakes, colorPallet){
 /*      Interaction!      */
 /* ====================== */
 
-window.onload = function(){
-  var game = new Game(document.getElementById('canvas'), tps, [new Snake(3, new Point(1, 2)), new Snake(3, new Point(1, 5))], color);
+// Movement / Controls
+function changeDir(e){
+  for (var i = 0; i < keybinds.length; i++) {
+    switch (e.keyCode) {
+      case keybinds[i].up:
+        game.snake[i].velocity.y === 1 ? true : game.snake[i].velocity = new Point(0, -1);
+        if (speedUp) {
+          game.tick();
+          game.draw();
+        }
+        break;
+      case keybinds[i].down:
+        game.snake[i].velocity.y === -1 ? true : game.snake[i].velocity = new Point(0, 1);
+        if (speedUp) {
+          game.tick();
+          game.draw();
+        }
+        break;
+      case keybinds[i].left:
+        game.snake[i].velocity.x === 1 ? true : game.snake[i].velocity = new Point(-1, 0);
+        if (speedUp) {
+          game.tick();
+          game.draw();
+        }
+        break;
+      case keybinds[i].right:
+        game.snake[i].velocity.x === -1 ? true : game.snake[i].velocity = new Point(1, 0);
+        if (speedUp) {
+          game.tick();
+          game.draw();
+        }
+        break;
+      default:
+    }
+  }
+}
+
+function startGame(snakes){
+  game = new Game(document.getElementById('canvas'), tps, snakes, color);
 
   setInterval(function(){
     game.tick();
@@ -179,39 +230,61 @@ window.onload = function(){
 
   document.addEventListener('keyup', changeDir);
   document.addEventListener('keydown', changeDir);
-  function changeDir(e){
-    for (var i = 0; i < keybinds.length; i++) {
-      switch (e.keyCode) {
-        case keybinds[i].up:
-          game.snake[i].velocity.y === 1 ? true : game.snake[i].velocity = new Point(0, -1);
-          if (speedUp) {
-            game.tick();
-            game.draw();
-          }
-          break;
-        case keybinds[i].down:
-          game.snake[i].velocity.y === -1 ? true : game.snake[i].velocity = new Point(0, 1);
-          if (speedUp) {
-            game.tick();
-            game.draw();
-          }
-          break;
-        case keybinds[i].left:
-          game.snake[i].velocity.x === 1 ? true : game.snake[i].velocity = new Point(-1, 0);
-          if (speedUp) {
-            game.tick();
-            game.draw();
-          }
-          break;
-        case keybinds[i].right:
-          game.snake[i].velocity.x === -1 ? true : game.snake[i].velocity = new Point(1, 0);
-          if (speedUp) {
-            game.tick();
-            game.draw();
-          }
-          break;
-        default:
-      }
-    }
+}
+
+/* ====================== */
+/*           UI           */
+/* ====================== */
+window.onload = function(){
+  document.getElementById('btn-single').onclick = function(){
+    //[new Snake(3, new Point(1, 2)), new Snake(3, new Point(1, 5)), new Snake(3, new Point(1, 8)), new Snake(3, new Point(1, 11)), new Snake(3, new Point(1, 14))]
+    document.getElementById('canvas').style.display = 'block';
+    document.getElementById('menu').style.display = 'none';
+    tps = parseInt(document.getElementById('tps').value);
+    speedUp = document.getElementById('speed-up').checked;
+    startGame([new Snake(3, new Point(1, 2))]);
   }
+  document.getElementById('btn-multi').onclick = function(){
+    //[new Snake(3, new Point(1, 2)), new Snake(3, new Point(1, 5)), new Snake(3, new Point(1, 8)), new Snake(3, new Point(1, 11)), new Snake(3, new Point(1, 14))]
+    //get the values
+    tps = parseInt(document.getElementById('tps').value);
+    speedUp = document.getElementById('speed-up').checked;
+    let menu = document.getElementById('menu');
+
+    //Hide the components
+    document.getElementById('common-settings').style.display = 'none';
+
+    //Generate config UI
+    let players = document.createElement('INPUT');
+    players.setAttribute('type', 'number');
+    players.setAttribute('min', '1');
+    players.setAttribute('max', '5');
+    players.setAttribute('value', '1');
+
+    let next = document.createElement('INPUT');
+    next.setAttribute('type', 'button');
+    next.setAttribute('value', 'Done');
+
+    //Confirm number entered
+    next.onclick = function(){
+      document.getElementById('canvas').style.display = 'block';
+      document.getElementById('menu').style.display = 'none';
+      let numPlayers = parseInt(players.value);
+      if (numPlayers > 5 || numPlayers < 1) {
+        alert('Invalid number - only values 1 to 5');
+        numPlayers = 1;
+      }
+      let snakeArray = [];
+      for (var i = 0; i < numPlayers; i++) {
+        snakeArray.push(new Snake(3, new Point(1, 2*i)));
+      }
+      startGame(snakeArray);
+    }
+
+    menu.textContent = 'Enter number of players: ';
+    //Append the UI Elements
+    menu.appendChild(players);
+    menu.appendChild(next);
+  }
+
 }
